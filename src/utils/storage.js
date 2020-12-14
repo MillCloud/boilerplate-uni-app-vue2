3f12036e518bbc21229e362cc6f6d86f2be94c1b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-12-14 19:36:11 +0800

    feat: add utils/storage function clear

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,13 +1,19 @@
 const keyToken = 'token';
 
+/**
+ * @return {string} token
+ */
 export function getToken() {
   return uni.getStorageSync(keyToken) || '';
 }
 
 /**
- *
  * @param {string} token
  */
 export function setToken(token) {
   return uni.setStorageSync(keyToken, token);
+}
+
+export function clear() {
+  return uni.clearStorageSync();
 }
