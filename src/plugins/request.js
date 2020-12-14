70efe58d2fd6b6782568743693f7fde7f860ba05
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-12-14 20:09:52 +0800

    feat: add request

--- src/plugins/request.js
+++ src/plugins/request.js
@@ -4,88 +4,112 @@ import { getToken } from '@u/storage';
 import { showModal } from '@u/modal';
 import packageInfo from '../../package.json';
 
+// https://uniajax.ponjs.com/
+// 要取消请求，参考 https://uniajax.ponjs.com/usage.html#requesttask
+
 const reLaunchCodes = new Set(['YOUR_RELAUNCH_CODE']);
 
-const request = ajax.create({
-  baseURL: 'https://your.url.here',
+const objectStatusCode = {
+  403: '不允许访问',
+  404: '请求的接口不存在',
+  500: '服务器内部错误',
+};
+
+const handleValidateStatusCode = (statusCode) =>
+  (statusCode >= 200 && statusCode < 300) || statusCode === 304;
+
+const handleShowError = (response) => {
+  if (reLaunchCodes.has(response.code)) {
+    showModal({
+      content: '请重新登录。',
+      success: () => {
+        uni.clearStorageSync();
+        uni.reLaunch({
+          url: '/pages/index/index',
+        });
+      },
+    });
+  } else {
+    showModal({
+      content: response.message,
+    });
+  }
+};
+
+const instance = ajax.create({
+  baseURL: process.env.VUE_APP_BASE_URL,
   header: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
-    'X-Version': `boilerplate-uni-app/${packageInfo.version}`,
+    'X-Version': `${packageInfo.name}/${packageInfo.version}`,
   },
+  withCredentials: false,
+  dataType: 'json',
+  responseType: 'text',
   sslVerify: false,
 });
 
-// 添加请求拦截器
-request.interceptors.request.use((config) => ({
-  ...config,
-  token: getToken(),
-}));
+instance.interceptors.request.use((config) => {
+  const token = getToken();
+  return {
+    ...config,
+    header: {
+      ...config.header,
+      'X-Token': token || '',
+    },
+  };
+});
 
-// 添加响应拦截器
-request.interceptors.response.use(
+instance.interceptors.response.use(
   (response) => {
-    // if (process.env.NODE_ENV === 'development') {
-    //   console.log('response', response.config.url.slice(33), response.data);
-    // }
-    if (reLaunchCodes.has(response.code)) {
-      showModal({
-        content: '请重新登录。',
-        success: () => {
-          uni.clearStorageSync();
-          uni.reLaunch({
-            url: '/pages/index',
-          });
-        },
-      });
+    const { data } = response;
+    if (!data.success) {
+      handleShowError(data);
     }
     return response.data;
   },
   (error) => {
-    // if (process.env.NODE_ENV === 'development') {
-    //   console.log('error', error);
-    // }
-    const customError = {
+    if (error.errMsg.toLowerCase().includes('cancel')) {
+      return {
+        success: false,
+        message: '请求已取消',
+        code: 'REQUEST_CANCELLED',
+      };
+    }
+    const response = {
       success: false,
-      code: '',
       message: '',
+      code: '',
     };
-    if (error.errMsg.toLowerCase().includes('timeout')) {
-      customError.message = '请求时间过长，请检查网络，稍后再试。';
+    if (!handleValidateStatusCode(error.statusCode)) {
+      // 状态码不正常
+      response.message =
+        objectStatusCode[error.statusCode] ||
+        `发生了错误，状态码${error.statusCode}`;
+      response.code = 'INVALID_STATUS_CODE';
     } else if (
-      error.statusCode < 200 ||
-      (error.statusCode >= 300 && error.statusCode !== 304)
+      error.errMsg.toUpperCase().includes('TIMEOUT') ||
+      error.errMsg.toUpperCase().includes('CONNRESET')
     ) {
-      // 发送了请求，响应超出范围 [200, 300) ∪ 304
-      customError.code = error.statusCode;
-      switch (error.statusCode) {
-        case 403:
-          customError.message =
-            '不能访问请求的内容，请联系维护并提供相关信息，谢谢。';
-          break;
-        case 404:
-          customError.message =
-            '请求内容不存在，请联系维护并提供相关信息，谢谢。';
-          break;
-        case 500:
-          customError.message = '服务器内部发生错误，请稍后再试。';
-          break;
-        default:
-          break;
-      }
-    } else if (Object.keys(error.data).length === 0) {
+      // 超时
+      response.message = '请求超时';
+      response.code = 'REQUEST_TIMEOUT';
+    } else if (error.data && Object.keys(error.data).length === 0) {
       // 发送了请求，没有收到响应
-      customError.message = '服务器无响应，请稍后再试。';
+      response.message = '服务器无响应，请稍后再试。';
+      response.code = 'NO_REQUEST';
     } else {
       // 请求时发生错误
-      customError.message =
-        '请求内容发生了问题，请联系维护并提供相关信息，谢谢。';
+      response.message = '请求错误';
+      response.code = 'REQUEST_ERROR';
     }
-    return customError;
+    // 处理错误
+    handleShowError(response);
+    return response;
   },
 );
 
-Vue.prototype.$request = request;
+Vue.prototype.$request = instance;
 
-export default request;
+export default instance;
