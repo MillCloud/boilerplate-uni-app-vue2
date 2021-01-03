e5b9ccd46e0dbf1e15cfab98d2413c0d121d3d21
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-01-03 17:36:28 +0800

    feat: update storage util

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,19 +1,27 @@
 const keyToken = 'token';
 
-/**
- * @return {string} token
- */
+/** @return {string} token */
 export function getToken() {
   return uni.getStorageSync(keyToken) || '';
 }
 
-/**
- * @param {string} token
- */
+/** @param {string} token */
 export function setToken(token) {
   return uni.setStorageSync(keyToken, token);
 }
 
 export function clear() {
   return uni.clearStorageSync();
+}
+
+const keyLanguage = 'language';
+
+/** @return {string} language */
+export function getLanguage() {
+  return uni.getStorageSync(keyLanguage) || process.env.VUE_APP_I18N_LOCALE;
+}
+
+/** @param {string} language */
+export function setLanguage(language = process.env.VUE_APP_I18N_LOCALE) {
+  return uni.setStorageSync(keyLanguage, language);
 }
