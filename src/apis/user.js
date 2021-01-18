a405b85fe93f77ea0074a5e2b0456d6bed981a69
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-01-19 00:20:31 +0800

    feat: add showError examples

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,22 +1,47 @@
 import request from '@/plugins/request';
 
-export function signIn({ username, password }) {
-  return request('/api/sign-in', {
-    username,
-    password,
+export function signIn({ username, password, showError = true } = {}) {
+  return request({
+    method: 'POST',
+    url: '/api/sign-in',
+    data: {
+      username,
+      password,
+    },
+    showError,
   });
 }
 
-export function signUp({ email, username, nickname, password }) {
-  return request('/api/sign-up', {
-    email,
-    username,
-    nickname,
-    password,
+export function signUp({
+  email,
+  username,
+  nickname,
+  password,
+  showError = true,
+} = {}) {
+  return request({
+    method: 'POST',
+    url: '/api/sign-up',
+    data: {
+      email,
+      username,
+      nickname,
+      password,
+    },
+    showError,
+  });
+}
+
+export function renew({ showError = true } = {}) {
+  return request({
+    method: 'POST',
+    url: '/api/renew',
+    showError,
   });
 }
 
 export default {
   signIn,
   signUp,
+  renew,
 };
