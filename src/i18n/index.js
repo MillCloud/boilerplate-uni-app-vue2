a841cccb49172ca198c0577cb59d84094fe6c788
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-17 16:50:08 +0800

    fix: fix i18n default language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
+import { getLanguage } from '@u/storage';
 
 Vue.use(VueI18n);
 
@@ -19,7 +20,7 @@ function loadLocaleMessages() {
 }
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'zh-Hans',
+  locale: getLanguage() || process.env.VUE_APP_I18N_LOCALE || 'zh-Hans',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'zh-Hans',
   messages: loadLocaleMessages(),
 });
