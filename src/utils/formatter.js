a8ee08c42e8fb6462ba09a758dad1716405b127f
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-17 16:50:47 +0800

    feat(utils): match ts

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,11 +1,21 @@
+import { isNumber } from 'lodash-es';
+
 /**
  * @param {string|number} amountYuan
  */
-export const formatYuan2Fen = (amountYuan) =>
-  Number.parseInt((Number.parseFloat(amountYuan) * 100).toFixed(0), 10);
+export const formatYuan2Fen = (amountYuan) => {
+  if (isNumber(amountYuan)) {
+    return Number.parseInt((amountYuan * 100).toFixed(0), 10);
+  }
+  return Number.parseInt((Number.parseFloat(amountYuan) * 100).toFixed(0), 10);
+};
 
 /**
  * @param {string|number} amountFen
  */
-export const formatFen2Yuan = (amountFen) =>
-  Number.parseFloat((Number.parseFloat(amountFen) / 100).toFixed(2));
+export const formatFen2Yuan = (amountFen) => {
+  if (isNumber(amountFen)) {
+    return Number.parseFloat((amountFen / 100).toFixed(2));
+  }
+  return Number.parseFloat((Number.parseFloat(amountFen) / 100).toFixed(2));
+};
