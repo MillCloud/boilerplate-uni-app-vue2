50d1ad148acb49447616c4cb7833bd96cc4f0f47
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-04-01 08:33:40 +0800

    fix: fix lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.json': 'prettier --write',
-  '*.{md,markdown}': 'markdownlint --fix && lint-md . --fix',
+  '*.{md,markdown}': 'markdownlint --fix && lint-md --fix',
   '*.{js,jsx,ts,tsx,vue}': 'vue-cli-service lint --fix',
   '*.{css,less,sass,scss,vue}': 'stylelint --fix',
 };
