42496d2827b6c5ee70f7e254225ef5c14f0d7243
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-09 13:56:14 +0800

    chore: update config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.json': 'prettier --write',
   '*.{css,less,sass,scss,vue}': 'stylelint --fix',
   '*.{js,jsx,ts,tsx,vue}': 'vue-cli-service lint --fix',
-  '*.{md,markdown}': 'markdownlint --fix && lint-md --fix',
+  '*.{md,markdown}': 'markdownlint --fix',
 };
