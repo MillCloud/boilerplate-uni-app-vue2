8a40b91917fe1c17a7a4a3829fac83696af3da32
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-09 14:14:59 +0800

    feat: add uni-composition-api

--- src/plugins/composition-api.js
+++ src/plugins/composition-api.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
+import UniCompositionAPI from 'uni-composition-api';
 
 Vue.use(VueCompositionAPI);
+Vue.use(UniCompositionAPI);
