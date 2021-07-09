3983e7bc548f50a9d8ccd99c4fa21cf103550ba1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-09 14:14:29 +0800

    style: format

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script>
 export default {
-  onLaunch() {
-    console.log('App Launch');
-  },
-  onShow() {
-    console.log('App Show');
-  },
-  onHide() {
-    console.log('App Hide');
-  },
+  onLaunch() {},
+  onShow() {},
+  onHide() {},
 };
 </script>
 
