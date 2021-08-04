d30f9191ee825449bab892ea20513db0ff3c5153
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-04 09:22:28 +0800

    chore: udpate config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
+    '@@/(.*)$': '<rootDir>/$1',
   },
   transformIgnorePatterns: ['/node_modules/(?!lodash-es)'],
 };
