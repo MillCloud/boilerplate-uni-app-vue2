7536c9cf8e9a01308ccdd61d695ac4d2ea7f5ce0
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-04 08:54:02 +0800

    feat: use dayjs zh-cn locale and customParseFormat plugin

--- src/plugins/dayjs.js
+++ src/plugins/dayjs.js
@@ -1,5 +1,11 @@
 import dayjs from 'dayjs';
 import Vue from 'vue';
+import 'dayjs/locale/zh-cn';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.locale('zh-cn');
+
+dayjs.extend(customParseFormat);
 
 Vue.prototype.$dayjs = dayjs;
 
