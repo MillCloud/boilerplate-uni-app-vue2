8ca69b5a6ddaca02b280ffc135fe329d9d1d4235
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-19 10:06:03 +0800

    chore: use pnpm

--- .release-it.js
+++ .release-it.js
@@ -13,7 +13,7 @@ module.exports = {
     release: false,
   },
   hooks: {
-    'before:init': 'yarn run lint',
+    'before:init': 'pnpm run lint',
     'after:bump': 'node update-manifest.js',
   },
 };
