5cb9d119b3832fe311ad54e44946ef73e82d79e2
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-09-16 12:09:50 +0800

    chore: update example

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
-<script lang="ts">
-import { defineComponent } from '@vue/composition-api';
-
-export default defineComponent({
+<script>
+export default {
   onLaunch() {},
   onShow() {},
   onHide() {},
-});
+};
 </script>
-
-<!-- <script setup lang="ts">
-import { onLaunch, onShow, onHide } from 'uni-composition-api';
-
-onLaunch(() => {});
-
-onShow(() => {});
-
-onHide(() => {});
-</script> -->
 
 <style lang="scss">
 @import './styles/variables';
