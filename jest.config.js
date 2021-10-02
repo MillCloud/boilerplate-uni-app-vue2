9f80fc3e455af4e74897194e6dcffe99a967b9ea
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-10-02 14:19:33 +0800

    chore: remove jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
-  },
-  transform: {
-    '.*\\.(vue)$': 'unplugin-vue2-script-setup/jest',
-  },
-};
