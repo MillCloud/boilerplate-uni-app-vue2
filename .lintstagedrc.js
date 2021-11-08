371266d603f2a03b6f7ad280751a26eca6cb1320
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-11-08 17:54:04 +0800

    chore: update config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
+  '*.md': 'markdownlint --fix',
   '*.{css,less,scss,vue}': 'stylelint --fix',
   '*.{js,jsx,ts,tsx,vue}': 'vue-cli-service lint --fix',
-  '*.{md,markdown}': 'markdownlint --fix',
 };
