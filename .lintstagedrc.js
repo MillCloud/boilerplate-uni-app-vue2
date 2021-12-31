cc6dd612a39b29cafca68cc4c758fcc6f0b279d5
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-31 16:15:04 +0800

    chore: update config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.md': 'markdownlint --fix',
   '*.{css,less,scss,sass,vue,svelte}': 'stylelint --fix',
-  '*.{js,jsx,ts,tsx,vue,svelte}': 'eslint --fix',
+  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue,svelte}': 'eslint --fix',
 };
