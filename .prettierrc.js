cb3d3fb4827ca1a44631b47c2a7e5fe033dab411
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-09 16:11:15 +0800

    chore: update config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,34 @@
-const { prettier } = require('@modyqyw/fabric');
-
 module.exports = {
-  ...prettier,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  plugins: [
+    './node_modules/prettier-plugin-jsdoc',
+    './node_modules/prettier-plugin-packagejson',
+    './node_modules/prettier-plugin-tailwindcss',
+  ],
+  overrides: [
+    {
+      files: ['*.css', '*.less', '*.scss', '*.sass'],
+      options: {
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
 };
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,57 @@
-const { stylelint } = require('@modyqyw/fabric');
-
 module.exports = {
-  ...stylelint.scss,
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recess-order',
+    'stylelint-prettier/recommended',
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'value'],
+      },
+    ],
+    'color-function-notation': null,
+    'comment-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['stylelint-commands'],
+        ignoreComments: [/^#ifdef/, /^#ifndef/, /^#endif/],
+      },
+    ],
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['composes', 'compose-with'],
+        ignoreSelectors: [':export', /^:import/],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['export', 'import', 'deep', 'slotted', 'global', 'local', 'external'],
+      },
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      { ignorePseudoElements: ['v-deep', 'v-slotted', 'v-global'] },
+    ],
+    'selector-type-no-unknown': null,
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'value-keyword-case': [
+      'lower',
+      {
+        ignoreFunctions: ['v-bind'],
+        ignoreProperties: ['font-family'],
+      },
+    ],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'value'],
+      },
+    ],
+  },
 };
